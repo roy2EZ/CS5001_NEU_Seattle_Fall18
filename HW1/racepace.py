ddac29ca89aa358b42d9c1c9e88b79db6075b1c7
Roy Chen <chen.rongy@husky.neu.edu>
2018-09-24 19:10:11 -0700

    Updated with comments
    added more comments to explain the code

--- HW1/racepace.py
+++ HW1/racepace.py
@@ -12,18 +12,29 @@
 #  racepace(5,0,31) => 3.11 miles,  9:58, 6.01 mph
 
 import math
-def racepace(distanceInKm,hour,min):
-    distanceInMile = distanceInKm/1.61
-    totalhour=hour+min/60
-    totalmin=hour*60+min
-    pacemin=math.floor(totalmin/distanceInMile)
-    pacesec=math.floor((totalmin/distanceInMile-pacemin)*60)
-    speed=round(distanceInMile/totalhour,2)
-    return "You ran " + str(round(distanceInMile,2))+" miles.\n"\
-    +"Your pace is "+str(pacemin)+":%02d"%(pacesec) +" per mile.\n"\
+def racepace(distance_in_km,hour,min):
+    distance_in_mile = distance_in_km/1.61
+    #convert kilometers to miles
+    total_hour=hour+min/60
+    #calculate total time in hours
+    total_min=hour*60+min
+    #calculate total time in minutes
+    pace_min=math.floor(total_min/distance_in_mile)
+    #calculate the minute part in final result of pace of fomation mm:ss
+    pace_sec=math.floor((total_min/distance_in_mile-pace_min)*60)
+    #calculate the second part in final result of pace of fomation mm:ss
+    speed=round(distance_in_mile/total_hour,2)
+    #calculate the speed
+    return "You ran " + str(round(distance_in_mile,2))+" miles.\n"\
+    +"Your pace is "+str(pace_min)+":%02d"%(pace_sec) +" per mile.\n"\
     +"Your speed is "+ str(speed)+" mph."
+    #return final result
 
 d = input("How many kilometers did you run?\n")
+#let user input distance in km
 h = input("How many hours did it take you?\n")
+#let user input the hour part of running time
 m = input("How many minutes?\n")
+#let user input the minute part of running time
 print(racepace(float(d),float(h),float(m)))
+#give final result
