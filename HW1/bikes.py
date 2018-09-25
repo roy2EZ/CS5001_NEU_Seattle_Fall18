900c5afcd9707d47b46bbd4ff20e8c9e362046f0
Roy Chen <chen.rongy@husky.neu.edu>
2018-09-24 19:00:42 -0700

    Add comment
    add more comment to explain the code

--- HW1/bikes.py
+++ HW1/bikes.py
@@ -37,16 +37,22 @@ def bikemaker(wheels, frames, links):
     #Calculate how many frames left 
     l_left = links-cl*bikes_possible
     #Calculate how many links left 
-
     return "OK, you've got "+str(bikes_possible)+" bikes coming!\n"\
     +"I'm keeping the leftovers for myself:\n"\
     +str(w_left)+" wheels and "+str(f_left)+" frames and "+str(l_left)+" links"
+    #return the final results
 
 w_input = input("How many wheels do you have?\n")
+#Let user input wheels amount
 print("I can make "+str(int(w_input)//2)+" bikes with that.\n")
+#calculate bikes amount using those wheels and print the result
 f_input = input("How many frames do you have?\n")
+#Let user input frames amount
 print("I can make "+str(int(f_input)//1)+" bikes with that.\n")
+#calculate bikes amount using those frames and print the result
 l_input  = input("How many links do you have?\n")
+#Let user input links amount
 print("I can make "+str(int(l_input)//50)+" bikes with that.\n")
-
+#calculate bikes amount using those links and print the result
 print(bikemaker(int(w_input), int(f_input), int(l_input)))
+#with all inputs, do the calculation and give final results
