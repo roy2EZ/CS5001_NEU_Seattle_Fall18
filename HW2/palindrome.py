a09745d2e4a01224344f8747bbc88e9b8f4be092
Roy Chen <chen.rongy@husky.neu.edu>
2018-09-30 20:18:45 -0700

    HW2 palindrome

---
+++ HW2/palindrome.py
@@ -0,0 +1,33 @@
+#### Purpose
+# To prompt the user for a string and reports whether it is a palindrome
+# The input is case-insensitive and space-insensitive
+#### Signature
+# palindrome_check :: (String) => String
+#### Template
+# def palindrome_check(given):
+#    return returns...
+#### Examples
+#  Madam Im Adam         => Palindrome!
+#  上海自来水来自海上      => Palindrome!
+#  Taco cat              => Palindrome!
+#  ?Was it a cat I saw?  => Palindrome!
+#  !!Sex at noon taxes!! => Palindrome!
+#  Madam, I'm Adam.      => Not a palindrome :(
+#  taco cat!             => Not a palindrome :(
+#  Not nil: Clinton!     => Not a palindrome :(
+#  No x in Nixon.        => Not a palindrome :(
+#  自我突破，突破自我     => Not a palindrome :(
+def palindrome_check(string):
+    string = list(string.replace(' ', '').upper())
+    length = len(string)
+    left = 0
+    right = length - 1
+    while left < right:
+        if string[left] != string[right]:
+            return "Not a palindrome :("
+        left += 1
+        right -= 1
+    return "Palindrome!"
+
+s = input("Please enter a word to check if it is palindrome:\n")
+print(palindrome_check(s))
