3dcccad07be2b9f5b88b827a247372dc2e845fd1
Roy Chen <chen.rongy@husky.neu.edu>
2018-10-01 13:03:34 -0700

    UPDATE

--- HW2/haybale.py
+++ HW2/haybale.py
@@ -30,37 +30,48 @@ WEEKEND_LIST = ["sat","sun"]
 DAY_LIST=WEEKDAY_LIST+WEEKEND_LIST
 
 def haybalemase_bill(temp,day,time,if_rain):
-    
+    #set up inital variable in the final results 
     temp_extra = 0
     day_extra = 0
     weather_extra=0
-
+    
+    #when temperature input is invalid, force default it as the max temperature 75F
     if temp < 0 & temp > 99:
         temp=MAX_TEMP
-        if temp>=MAX_TEMP:
-            temp_extra = (temp - MAX_TEMP)*ABOVE_TEMP_RATE
-        elif temp<=MIN_TEMP:
-            temp_extra = (MIN_TEMP - temp)*BELOW_TEMP_RATE
+        #when temperature is over max, every degree will charge an rate as ABOVE_TEMP_RATE
+    if temp>=MAX_TEMP and temp<=99:
+        temp_extra = (temp - MAX_TEMP)*ABOVE_TEMP_RATE
+        #when temperature is below min, every degree will apply a discount rate as BELOW_TEMP_RATE    
+    elif temp<=MIN_TEMP and temp>=0:
+        temp_extra = (MIN_TEMP - temp)*BELOW_TEMP_RATE
     
-    
+    #when the day input is invalid, force default the input as "mon"
     if day not in DAY_LIST:
         day="mon"
-        if day in WEEKDAY_LIST:
-            if time <0 or time>23:
-                time=12
-                if time >= WEEKADY_LATE_TIME:
-                    day_extra = WEEKDAY_LATE_EXTRA
-                else:
-                    day_extra = 0
-        elif day in WEEKEND_LIST:
-            day_extra = WEEKEND_EXTRA
+        #when the day input is weekdays, continue to check the time
+    if day in WEEKDAY_LIST:
+        #when time input is invalid, force default it into 12:00
+        if time <0 or time>23:
+            time=12
+            #when the time is over 17:00, charge an extra late fee as WEEKDAY_LATE_EXTRA
+        if time >= WEEKADY_LATE_TIME and time<=23:
+            day_extra = WEEKDAY_LATE_EXTRA
+                #when the time is not over 17:00, no extra charge    
+        else:
+            day_extra = 0
+        #when day is weekend, charge a weekend extra            
+    elif day in WEEKEND_LIST:
+        day_extra = WEEKEND_EXTRA
     
+    #if it is raining, charge a weather extra fee
     if if_rain=="Y":
         weather_extra=RAIN_EXTRA
+    #if it is not raining or user input is invalid, default the input as not raining
+    # and don't charge weather extra fee    
     else:
         weather_extra=0
-
-    return "Your final price for the maze is: $"+str(round(BASE_PRICE+temp_extra+day_extra+weather_extra,2))
+    #return the final price with base price adding all extra fees, round the result with 2 digits of
+    return "Your final price for the maze is: $"+ str("%.2f" % (BASE_PRICE+temp_extra+day_extra+weather_extra))
 
 temp= int(input("What is the temprature now?\n"))
 day = input("What day of the week is it?\n").lower()
