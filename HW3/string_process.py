1f3f998abd92c65866dc821edd91e56cb067dcc2
Roy Chen <chen.rongy@husky.neu.edu>
2018-10-07 00:51:22 -0700

    update problem 2 and 3

--- HW3/string_process.py
+++ HW3/string_process.py
@@ -6,18 +6,25 @@
 # def to_upper(given...):
 # return returns...
 #### Examples
-# to_upper(“test string 9000”) => “TEST STRING 9000”
-# to_upper(“ThIs Is A sTrInG”) => “THIS IS A STRING”
+# to_upper("test string 9000") => "TEST STRING 9000"
+# to_upper("ThIs Is A sTrInG") => "THIS IS A STRING"
 
 def to_upper(string):  
+    #create a list of input string
     s1=list(string)
+    #copy a list s2 from s1 and stand-by as an output
     s2=list(s1)
+    #initial i as 0 
     i=0
     while i<len(s1):
+        #in Unicode, the code point of lowercase a to z is from 97 to 122
+        #if the input is in that range then transfer it to uppercase letter range by minus 32 
         if ord(s1[i]) >= 97 and ord(s1[i]) <= 122:
             s2[i]=str(chr(ord(s1[i]) - 32))   
         i += 1
+    #join each element in the list s2 into a str as output    
     return "".join(s2)
+
 
 
 #### Purpose
@@ -28,13 +35,18 @@ def to_upper(string):
 # def to_lower(given...):
 # return returns...
 #### Examples
-# to_lower(“SHOUTY STRING”) => “shouty string”
-# to_lower(“ThIs Is A sTrInG”) => “this is a string”
-def to_lower(string):   
+# to_lower("SHOUTY STRING") => "shouty string"
+# to_lower("ThIs Is A sTrInG") => "this is a string"
+def to_lower(string): 
+    #create a list s1 of input string  
     s1=list(string)
+    #copy a list s2 from s1 and stand-by as an output
     s2=list(s1)
+    #initial i as 0
     i=0
     while i<len(s1):
+        #in Unicode, the code point of uppercase A to Z is from 65 to 90
+        #if the input is in that range then transfer it to lowercase letter range by adding 32 
         if ord(s1[i]) >= 65 and ord(s1[i]) <= 90:
             s2[i]=str(chr(ord(s1[i]) + 32))  
         i += 1
@@ -50,34 +62,57 @@ def to_lower(string):
 # def in_reverse(given…):
 # return returns...
 #### Examples
-# in_reverse(“Tuesday 3:00 PM”) => “MP 00:3 yadseuT”
-# in_reverse(“abcde”) => “edcba”
+# in_reverse("Tuesday 3:00 PM") => "MP 00:3 yadseuT"
+# in_reverse("abcde") => "edcba"
 def in_reverse(string): 
+    #create a list s1 of input string  
     s1=list(string)
+    #copy a list s2 from s1 and stand-by as an output
     s2=list(s1)
+    #initial i as 0
     i=0
     while i<len(s1):
-        s2[i]=s1[-i-1]    
+        #make the element s2[i] as same as the element on s1[-1-i] 
+        s2[i]=s1[-1-i]    
         i += 1
     return  "".join(s2)
 
-
+#### Purpose
+# This function takes a string as input and returns palindrome using that user input, 
+# with all uppercase or lowercase option for user to choose
+#
+#### Signature
+# string_process :: (String,String) => String
+#### Template
+# def string_process(given…):
+# return returns...
+#### Examples
+# Testcase1:
+# Make Me A Palindrome! 
+# U
+# => MAKE ME A PALINDROME!!EMORDNILAP A EM EKAM
+# Testcase2:
+# 0123ABC
+# L
+# => MAKE ME A PALINDROME!!EMORDNILAP A EM EKAM
+# Testcase1:
+# STOP
+# => OK, stopping.
 def string_process():
     str_input = input("Please enter a string to convert:\n")
     str_palindrome = in_reverse(str_input)
     str_result = str_input + str_palindrome
-    
     if str_input == "STOP":
-        return "OK, STOP"
+        return "OK, stopping."
     else:    
         print("Would you like and uppercase or lowercase palindrome?") 
         case_user_requir = input("Enter U for uppercase and L for lowercase.\n")
         if case_user_requir == "U":
-            print(to_upper(str_result))
+            print("Here is your uppercase palindrome:\n"+to_upper(str_result))
             return string_process()
         elif case_user_requir == "L":
-            print(to_lower(str_result))
+            print("Here is your lowercase palindrome:\n"+to_lower(str_result))
             return string_process()
-    
-print(string_process())  
-    
+
+
+print(string_process())
---
+++ HW3/upc.py
@@ -0,0 +1,55 @@
+#### Purpose
+# This function prompt user enter a  and calculate if the
+#### Signature
+# in_reverse :: String => String
+#### Template
+# def in_reverse(given…):
+# return returns...
+#### Examples
+# in_reverse(“Tuesday 3:00 PM”) => “MP 00:3 yadseuT”
+# in_reverse(“abcde”) => “edcba”
+# Test case #1: Meadowoods Air Fresshener
+#4973258253 -- valid
+#4973259253 -- invalid -- mistyped 8 instead of 9
+#4972358253 -- invalid -- transposed two digits
+
+# Test case #2: FredMeyer Purified Drinking Water 
+#924187053904 -- valid
+#924187054904 -- invalid -- mistyped 3 instead of 4
+#924178053904 -- invalid -- transposed two digits
+
+# Test case #3: Head First Python 2nd Edition
+#781421916537 -- valid
+#781521916537 -- invalid -- mistyped 4 instead of 5
+#781421196537 -- invalid -- transposed two digits
+
+def upc():
+    #Take user input string
+    str_input=input("Enter the UPC code:\n")
+    #reverse the user input string for read the UPC code from right end to left
+    str_reverse=str_input[::-1]
+    #set a initial result as 0
+    result = 0
+    #get the length for using as the max range in condition 
+    length = len(str_reverse)
+    #initial i as 0
+    i = 0
+    while i < length:
+        #Digits in even positions: add it to result
+        if i % 2 == 0:
+            result += int(str_reverse[i])
+            i+=1
+        #Digits in odd positions: multiply it by 3 and add it to result    
+        else:
+            result += 3*int(str_reverse[i])
+            i+=1
+    
+    #if this result is a multiple of 10, then it is valid UPC, output valid notification
+    if result % 10 == 0:
+        return "Valid UPC!!"
+    #if it is not, output not valid notification
+    else:
+        return "Not valid UPC code."
+    
+#test the function    
+print(upc())
