8bda8ee30bc22a95c9f02593ff955e82198d57ac
Roy Chen <chen.rongy@husky.neu.edu>
2018-10-18 20:26:51 -0700

    Update LAB5_10162018/caesar_cipher_part3.py

--- LAB5_10162018/caesar_cipher_part3.py
+++ LAB5_10162018/caesar_cipher_part3.py
@@ -10,9 +10,6 @@ def unknown_decryption_slide(ciphertext):
         list_slide.append(ciphertext_slide_back)
     return list_slide
 
-
-
-
 def unknown_decryption_shift(ciphertext):
     length = len(ciphertext)
     # To create a empty plaintext list for carrying future output
@@ -31,8 +28,6 @@ def unknown_decryption_shift(ciphertext):
         list_shift.append(plaintext_str)  
     return list_shift
 
-
-
 def main():
     ciphertext = input("Please enter the cipher text you wanna decrypt: \n")
     print("Matrix of results: ")
@@ -40,6 +35,21 @@ def main():
     for i in range(len(list_slide)):
         print(unknown_decryption_shift(list_slide[i]))
 
-    
+#### Tests
+# Cipher texts:
+# *.glygo rsvvmw'w gshi avmxiw gsqqirxw efsyx *lmq
+# aqwuvwem, kp cp kphkpkvg nqqr, ykvj
+# .sjajw ywzxy f htruzyjw dtz hfs’y ymwtb tzy f bnsitb..
+# yko.vjg swguvkqp qh yjgvjgt eqorwvgtu ecp vjkpm ku nkmg vjgswguvkqp qh yjgvjgt uwdoctkpgu ecp u
+# .ejwem pqttku ytkvgu dqqngcp gzrtguukqpu vjcv ctg dqvj vtwg cpfhcnug
+
+#### Results
+# chuck norris's code writes comments about *him*.
+# stuck, in an infinite loop, withyou
+# never trust a computer you can’t throw out a window...
+# the question of whether computers can think is like thequestion of whether submarines can swim.
+# chuck norris writes boolean expressions that are both true andfalse.
+
+
 
     
