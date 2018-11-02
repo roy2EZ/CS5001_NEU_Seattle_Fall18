602a90fb13f11c3db0378a4e980a8d912cbe26e8
Roy Chen <chen.rongy@husky.neu.edu>
2018-11-02 14:02:23 -0700

    Update HW5/snowflake.py

--- HW5/snowflake.py
+++ HW5/snowflake.py
@@ -4,12 +4,18 @@ turt.speed(200)
 
 #### PURPOSE
 # To draw one side of a koch curve
-#### Signature
-# koch :: (Integer,Integer) => Graph
-#### Template
+#### SIGNATURE
+# koch :: (Integer,Integer) => Void
+#### TEMPLATE
 # koch(given given…):
+#    return returns
 #### EXAMPLES
-# koch(100,1) => __/\__
+# koch(100,1) => 
+# the graph will be the basic unit of koch curve, such as _/\_
+# koch(100,2) => 
+# the graph will be the curve's recursion on itself
+# as a smaller triangle will be on each lines of the curve.
+
 def koch(size,order):
     if order <= 0:
         turt.fd(size)
@@ -24,18 +30,23 @@ def koch(size,order):
         
 #### PURPOSE
 # To draw the full koch snowflake
-#### Signature
-# snowflake :: (Integer,Integer,Integer) => Graph
-#### Template
+#### SIGNATURE
+# snowflake :: (Integer,Integer,Integer) => Void
+#### TEMPLATE
 # snowflake(given given…):
-           
+#    return returns
+#### EXAMPLES
+# snowflake(3,200,4) => 
+# the graph will be the snow flake with 3 sides of koch curve, 
+# each one is 120 degree with each other, and in each unit koch curve, 
+# there are 4 layers koch curve on itself.
+
 def snowflake(n,size,order):
     for i in range(n):
         koch(size,order)
         turt.right(360/n)
 
 
-#### Test
-
-snowflake(3,200,3)
+#### Test: run the function to draw the snow flake
+snowflake(3,200,4)
 turtle.done()
